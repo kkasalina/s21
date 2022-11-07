f7041f59ec89d545a266ece9598a00cc32291146
Ayrmidon Hagraven <ayrmidon@un-r2.kzn.21-school.ru>
2022-11-07 18:49:24 +0300

    done, no func

--- src/cat/func.c
+++ src/cat/func.c
@@ -1,4 +1,24 @@
-#include "s21_cat.h"
+// #ifndef SRC_CAT_S21_CAT_H_
+// #define SRC_CAT_S21_CAT_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+int ge;
+int b = 0, e = 0, n = 0, s = 0, t = 0, v = 0;
+
+static struct option long_options[] =
+    {
+        {"number-nonblank", 0, 0, 'b'},
+        {"number", 0, 0, 'n'},
+        {"squeeze-blank" , 0, 0, 's'}
+        
+    };
+
+void switch_f (int ge);
+void big_func (int argc, char *argv[], int b, int e, int n, int s, int t, int v);
+void file_open (int argc, char* argv[], int size, int filename);
+void size_f (FILE* file, int size);
 
 #define STR_SIZE 10000
 
@@ -8,54 +28,46 @@ int main(int argc, char *argv[])
     {
         while (((ge = getopt_long(argc, argv, "+beEnstTv?", long_options, NULL)) != -1))
         {
-            switch (ge)
-            {
-                case 'b': b++; break;
-                case 'v': v++; break;
-                case 'E':
-                case 'e': e++; break;
-                case 'n': n++; break;
-                case 's': s++; break;
-                case 'T':
-                case 't': t++; break;
-                case '?': 
-                default: printf("Флаг не распознан!\n"); exit(1);
-            }
+             switch_f (ge);
         }
-        FILE *file;
-        int filename = optind;
-            
-            
-        while (filename < argc)
-        {
-            file = fopen(argv[filename], "r");
-            if (file == NULL)
-            {
-                perror("Файл не найден");
-                exit(1);
-            }
+        printf("1111");
+        big_func (argc, argv, b, e, n, s, t, v);
+    printf("2222");
+    }
+    return 0;
+}
 
-            char f;
+void switch_f (int ge)
+{
+    switch (ge)
+    {
+        case 'b': b++; break;
+        case 'v': v++; break;
+        case 'E':
+        case 'e': e++; break;
+        case 'n': n++; break;
+        case 's': s++; break;
+        case 'T':
+        case 't': t++; break;
+        case '?': 
+        default: printf("Флаг не распознан!\n"); exit(1);
+    }  printf("333");
+}
+void big_func (int argc, char *argv[], int b, int e, int n, int s, int t, int v)
+{
+         printf("4444");
             int size = 0;
-            while ((f = getc(file)) != EOF)
-            {
-                size++;
-            
-            } 
-            fclose(file);
+            int filename = 0;
+            FILE* file;
+            file_open (argc, argv, size, filename);
             file = fopen(argv[filename], "r");
-           
             char lastSim = '\n';
             char lastLastSim = '0';
             char c = '0';
             char flag_t = '0';
             int flag_v = 0;
-            int flag_n = 1;
-            int flag_b = 1;
-            
-
             for (int symCount = 0; (c = getc(file)) != EOF; symCount++)
-            {   
+            {   printf("5555");
                 int endStr = 0;
                 if (c == '\n') endStr += 1;
                 if (s)
@@ -82,10 +94,10 @@ int main(int argc, char *argv[])
                 if (b != 0) n = 0;
                 if (n != 0 && b == 0)
                 {
-                    if (symCount == 0 || lastSim == 10)
+                    if (symCount == 0)
                     {
-                        printf("%6d\t", flag_n);
-                        flag_n++;
+                        printf("%6d\t", n);
+                        n++;
                     }
                 }
                 if (v == 1)
@@ -109,7 +121,8 @@ int main(int argc, char *argv[])
                 }  
                    
                     if (n != 0 && flag_v == 0 ) printf("%c", c);
-                    if (b != 0 && lastSim == '\n' && c != '\n') printf("%6d\t", flag_b++);
+                    if (n != 0 && endStr != 0 && (symCount != size - 1 && c == '\n'))  printf("%6d\t", n++);
+                    if (b != 0 && lastSim == '\n' && c != '\n') printf("%6d\t", b++);
                     if (v != 0 && flag_v != 0) printf("^");
 
                     if (n == 0 || flag_v != 0) printf("%c", c); 
@@ -121,12 +134,33 @@ int main(int argc, char *argv[])
                 endStr = 0;
                 flag_t = '0';
                 flag_v = 0;
-                if (symCount == size - 1) flag_n = 1;
+                if (symCount == size - 1) n = 1;
             }
-            
-        filename++;
-        fclose(file);
+            filename++;
+            fclose(file);
+}
+void file_open (int argc, char* argv[], int size, int filename)
+{   printf("6666");
+        FILE *file;
+        filename = optind;
+        while (filename < argc)
+        {
+            file = fopen(argv[filename], "r");
+            if (file == NULL)
+            {
+                perror("Файл не найден");
+                exit(1);
+            }
         }
-    }
-    return 0;
+            size_f(file, size);
+        
+}
+void size_f (FILE* file, int size)
+{ printf("777");
+    char f;
+    while ((f = getc(file)) != EOF)
+    {
+        size++;
+    } 
+    fclose(file);
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -2,9 +2,19 @@
 #define SRC_CAT_S21_CAT_H_
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h> // ? нужен ли? 
-#include <string.h> // ? нужен ли?  
-#define STR_SIZE 10000
+#include <getopt.h>
+
+int ge;
+int b = 0, e = 0, n = 0, s = 0, t = 0, v = 0;
+
+static struct option long_options[] =
+    {
+        {"number-nonblank", 0, 0, 'b'},
+        {"number", 0, 0, 'n'},
+        {"squeeze-blank" , 0, 0, 's'}
+        
+    };
+
 
 // int voidline (char c, int voidLine);
 // int count (char *str, int x);
