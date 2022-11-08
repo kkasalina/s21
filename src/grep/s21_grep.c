d590f31d9a2bfe709c11827957e2ac2137f15bec
Ayrmidon Hagraven <ayrmidon@un-r2.kzn.21-school.ru>
2022-11-08 18:53:07 +0300

    to bigfunc

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,68 +1,124 @@
 #include "s21_grep.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 1)
-    {
-        while ((gr = getopt(argc, argv, "+eivclnhsfo?"))) {
-            flags (gr, argv);
-        }
-        shablon_str(argv);
-        input_file_open(argc, argv, optind);
+int main(int argc, char* argv[]) {
+  if (argc != 1) {
+    while ((gr = getopt_long(argc, argv, "+e:f:ivclnhso?", long_options, NULL) !=
+                 -1)) {
+      flags(gr, argv);
     }
-    return 0;
-}  
+    shablon_str(argv);
+    input_file_open(argc, argv, optind);
+    // big_func();
+  }
+  return 0;
+}
 // чтение флагов
-void flags (int gr, char** argv)
-{
-    switch(gr) {
-        case 'e': e++; shablon_str(argv); break;
-        case 'i': i++; break;
-        case 'v': v++; break;
-        case 'c': c++; break;
-        case 'l': l++; break;
-        case 'n': n++; break;
-        case 'h': h++; break;
-        case 's': s++; break;
-        case 'f': f++; shablon_str(argv); break;
-        case 'o': o++; break;
-        case '?':
-        default: printf("Флаг не распознан!\n"); exit(1);
-    }
+void flags(int gr, char** argv) {
+  switch (gr) {
+    case 'e':
+      e++;
+      shablon_str(argv);
+      break;
+    case 'i':
+      i++;
+      break;
+    case 'v':
+      v++;
+      break;
+    case 'c':
+      c++;
+      break;
+    case 'l':
+      l++;
+      break;
+    case 'n':
+      n++;
+      break;
+    case 'h':
+      h++;
+      break;
+    case 's':
+      s++;
+      break;
+    case 'f':
+      f++;
+      shablon_str(argv);
+      break;
+    case 'o':
+      o++;
+      break;
+    case '?':
+    default:
+      printf("Флаг не распознан!\n");
+      exit(1);
+  }
 }
 // чтение шаблона из потока ввода
-void shablon_str(char** argv)
-{
-    char str[SIZE];
-    char buf[SIZE];
-    if (e == 1 && f == 0)
-    {
-        sprintf(shablon, SIZE, "%s", );
+void shablon_str(char** argv) {
+  char str[SIZE];
+  // char buf[SIZE];
+  if (e == 0 && f == 0) {
+    snprintf(shablon, SIZE, "%s", argv[optind++]);  // можно optarg?
+  }
+  if (e != 0 && f == 0) {
+    if (optarg == 0 || !shablon[0])
+      snprintf(shablon, SIZE, "%s", argv[optind]);  // нужно optarg
+    else {
+      snprintf(str, SIZE, " %s", argv[optind]);  // нужно optarg and | ?
+      strcat(shablon, str);
     }
-}
-// открытие и проверка файла, доработать 
-void input_file_open (int argc, char** argv, int optind)
-{
-    FILE *file;
-    int filename = optind;
-    char *str;
-    
-        for (str = argv[filename]; str != NULL; filename++)
-        {
-            if (str)
-            {
-                file = fopen(argv[filename], "r");
-                if (file == NULL) 
-                {
-                    if (s == 0)
-                    {
-                        perror("Файл не найден");
-                        exit(1);  
-                    }
-                }
-                void big_func ();
-            }
-            fclose(file); 
+  }
+  if (f && !e) {
+    char f_pattern[SIZE] = {0};
+    FILE* f_file;
+    char* f_str = optarg;
+    if (f_file == NULL) {
+      if (s == 0) {
+        perror("Файл не найден");
+        exit(1);
+      }
+    } else if (f_file) {
+      f_file = fopen(f_str, "r");
+      while ((fgets(f_pattern, SIZE, f_file)) != NULL)  // NULL?
+      {
+        // if (f_pattern[strlen(f_pattern) - 1] == 10) {
+        // f_pattern[strlen(f_pattern) - 1] = 0;
+        // }
+        if (optarg == 0 || !shablon[0])
+          snprintf(shablon, SIZE, "%s", f_pattern);  // нужно optarg
+        else {
+          snprintf(str, SIZE, " %s", f_pattern);  // нужно optarg and | ?
+          strcat(shablon, str);
         }
+        // счетчик i++ ?
+      }
+    }
+    fclose(f_file);  // нужно ли закрывать?
+  }
+}
+// открытие и проверка файла, доработать
+void input_file_open(int argc, char** argv, int optind) {
+  FILE* file;
+  int filename = optind;
+  char* str;
+
+  for (str = argv[filename]; str != NULL; filename++) {
+    if (str) {
+      file = fopen(argv[filename], "r");
+      if (file == NULL) {
+        if (s == 0) {
+          perror("Файл не найден");
+          exit(1);
+        }
+      }
+      void big_func();
+    }
+    fclose(file);
+  }
 }
 // функция взаимодействия флагов и печать
-void big_func ();
+void big_func(int argc, FILE* file, char** str) {
+   if (e) {
+
+   }
+}
--- src/grep/s21_grep.h
+++ src/grep/s21_grep.h
@@ -5,17 +5,23 @@
 #include <getopt.h> 
 #include <string.h> 
 #include <pcre.h>
+// #include <regex.h>
+#include <unistd.h>
 #define  _GNU_SOURCE
 #define SIZE 10000
 
 int gr, e = 0, i = 0, v = 0, c = 0, l = 0, n = 0, h = 0, s = 0, f = 0, o = 0;
-char shablon[SIZE];
+char shablon[10000];
+static struct option long_options[] =
+    {
+      {0, 0, 0, 0},
+    };
 
 void flags (int gr, char** argv);
 void input_file_open (int argc, char** argv, int optind);
 void shablon_str(char** argv);
 void big_func ();
-// #include <regex.h>
+
 
 
 #endif // SRC_CAT_S21_GREP_H_
