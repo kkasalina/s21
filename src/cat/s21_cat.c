5f7f3d3536f13e2e5cb9742b65fc330c60d873b6
Ayrmidon Hagraven <ayrmidon@pr-j2.kzn.21-school.ru>
2022-11-17 15:42:52 +0300

    Cat done

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,40 +1,41 @@
 #include "s21_cat.h"
 
-#define STR_SIZE 10000
-
 int main(int argc, char *argv[]) {
+  int ge;
+  unit flag = {0};
+  arr chars = {'\n', '0', '0', '0', 0, 1, 1};
   if (argc != 1) {
     while (((ge = getopt_long(argc, argv, "+beEnstTv?", long_options, NULL)) !=
             -1)) {
-      flags();
+      flags(ge, &flag);
     }
-    file_open(argc, argv, optind);
+    file_open(argc, argv, optind, &flag, &chars);
   }
 
   return 0;
 }
 
-void flags() {
+void flags(int ge, unit *flag) {
   switch (ge) {
     case 'b':
-      b++;
+      flag->b = 1;
       break;
     case 'v':
-      v++;
+      flag->v = 1;
       break;
     case 'E':
     case 'e':
-      e++;
+      flag->e = 1;
       break;
     case 'n':
-      n++;
+      flag->n = 1;
       break;
     case 's':
-      s++;
+      flag->s = 1;
       break;
     case 'T':
     case 't':
-      t++;
+      flag->t = 1;
       break;
     case '?':
     default:
@@ -43,7 +44,7 @@ void flags() {
   }
 }
 
-void file_open(int argc, char **argv, int optind) {
+void file_open(int argc, char **argv, int optind, unit *flag, arr *chars) {
   FILE *file;
   int filename = optind;
   while (filename < argc) {
@@ -53,62 +54,64 @@ void file_open(int argc, char **argv, int optind) {
       exit(1);
     }
     file = fopen(argv[filename], "r");
-    big(file);
+    big(file, flag, chars);
     filename++;
     fclose(file);
-    flag_n = 1;
+    chars->flag_n = 1;
   }
 }
 
-void big(FILE *file) {
-  for (int symCount = 0; (c = getc(file)) != EOF; symCount++) {
+void big(FILE *file, unit *flag, arr *chars) {
+  for (int symCount = 0; (chars->c = getc(file)) != EOF; symCount++) {
     int endStr = 0;
-    if (c == '\n') endStr += 1;
-    if (s) {
-      if (symCount == 1 && lastSim == '\n' && c == '\n') {
+    if (chars->c == '\n') endStr += 1;
+    if (flag->s) {
+      if (symCount == 1 && chars->lastSim == '\n' && chars->c == '\n') {
         continue;
       }
-      if (lastLastSim == '\n' && lastSim == '\n' && c == '\n') {
+      if (chars->lastLastSim == '\n' && chars->lastSim == '\n' &&
+          chars->c == '\n') {
         continue;
       }
     }
-    if (t != 0) {
-      v = 1;
-      if (c == '\t') {
-        flag_t = c;
-        c = '^';
+    if (flag->t != 0) {
+      flag->v = 1;
+      if (chars->c == '\t') {
+        chars->flag_t = chars->c;
+        chars->c = '^';
       }
     }
-    if (b != 0) n = 0;
-    if (n != 0 && b == 0) {
-      if (symCount == 0 || lastSim == 10) {
-        printf("%6d\t", flag_n);
-        flag_n++;
+    if (flag->b != 0) flag->n = 0;
+    if (flag->n != 0 && flag->b == 0) {
+      if (symCount == 0 || chars->lastSim == 10) {
+        printf("%6d\t", chars->flag_n);
+        chars->flag_n++;
       }
     }
-    if (v == 1) {
-      if (c < 32 && c != 9 && c != 10) {
-        c += 64;
-        flag_v++;
+    if (flag->v == 1) {
+      if (chars->c < 32 && chars->c != 9 && chars->c != 10) {
+        chars->c += 64;
+        chars->flag_v++;
       }
-      if (c == 127) {
-        c = '?';
-        flag_v++;
+      if (chars->c == 127) {
+        chars->c = '?';
+        chars->flag_v++;
       }
     }
-    if (e != 0 && c == '\n') {
-      v = 1;
+    if (flag->e != 0 && chars->c == '\n') {
+      flag->v = 1;
       printf("$");
     }
-    if (n != 0 && flag_v == 0) printf("%c", c);
-    if (b != 0 && lastSim == '\n' && c != '\n') printf("%6d\t", flag_b++);
-    if (v != 0 && flag_v != 0) printf("^");
-    if (n == 0 || flag_v != 0) printf("%c", c);
-    if (t != 0 && c == '^' && flag_t == '\t') printf("I");
-    lastLastSim = lastSim;
-    lastSim = c;
+    if (flag->n != 0 && chars->flag_v == 0) printf("%c", chars->c);
+    if (flag->b != 0 && chars->lastSim == '\n' && chars->c != '\n')
+      printf("%6d\t", chars->flag_b++);
+    if (flag->v != 0 && chars->flag_v != 0) printf("^");
+    if (flag->n == 0 || chars->flag_v != 0) printf("%c", chars->c);
+    if (flag->t != 0 && chars->c == '^' && chars->flag_t == '\t') printf("I");
+    chars->lastLastSim = chars->lastSim;
+    chars->lastSim = chars->c;
     endStr = 0;
-    flag_t = '0';
-    flag_v = 0;
+    chars->flag_t = '0';
+    chars->flag_v = 0;
   }
 }
--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -3,25 +3,38 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #define _GNU_SOURCE
 
-int ge;
-int b = 0, e = 0, n = 0, s = 0, t = 0, v = 0;
-char lastSim = '\n';
-char lastLastSim = '0';
-char c = '0';
-char flag_t = '0';
-int flag_v = 0;
-int flag_n = 1;
-int flag_b = 1;
+#define STR_SIZE 10000
+
+typedef struct {
+  int b;
+  int e;
+  int n;
+  int s;
+  int t;
+  int v;
+} unit;
+
+typedef struct {
+  char lastSim;
+  char lastLastSim;
+  char c;
+  char flag_t;
+  int flag_v;
+  int flag_n;
+  int flag_b;
+} arr;
+
 static struct option long_options[] = {{"number-nonblank", 0, 0, 'b'},
                                        {"number", 0, 0, 'n'},
                                        {"squeeze-blank", 0, 0, 's'}
 
 };
 
-void flags();
-void file_open(int argc, char **argv, int optind);
-void big(FILE *file);
+void flags(int ge, unit *flag);
+void file_open(int argc, char **argv, int optind, unit *flag, arr *chars);
+void big(FILE *file, unit *flag, arr *chars);
 
 #endif  // SRC_CAT_S21_CAT_H_
