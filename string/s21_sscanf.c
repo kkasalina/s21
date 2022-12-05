8235a296c29cd8f87d25da344dac72f3b9312409
Ayrmidon Hagraven <ayrmidon@pr-k4.kzn.21-school.ru>
2022-12-05 22:38:52 +0300

    v4

--- string/s21_sscanf.c
+++ string/s21_sscanf.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-static char *tokenstring = "hello 0";
+static char *tokenstring = "kifyui 12356789 ";
 char s[81];
 char *res = NULL;
-// char p[81] = {0};
-int p;
+int arr[50] = {0};
+int p = 0;
 size_t i = 0;
 size_t j = 0;
 
@@ -22,8 +22,30 @@ int formatFlags(const char *format) {
   return flagSpec;
 }
 
+int s21_atoi(const char *str, int *arr) { 
+  int leght = 0;
+  for (int k = 0; str[j] != '\0'; j++, k++) {
+    if (str[j] > 47 && str[j] < 58) {
+      arr[k] = str[j] - 48 ;
+      leght++;
+    } 
+  }
+  if (leght > 1) {
+    for (int k = 0; k <= leght; k++) {
+      p += arr[k];
+      p *= 10;
+    }
+      p /= 10;
+  } else {
+     p += arr[1];
+  }
+ j -= leght;
+  return p;
+}
+
 char *s21_sscanf(const char *str, const char *format, ...) {
   int leght = strlen(str);
+  // char* h =
   if (formatFlags(format) != 0) {
     while (leght != 0) {
       va_list arg;
@@ -32,20 +54,14 @@ char *s21_sscanf(const char *str, const char *format, ...) {
         if (format[i] == '%') {
           i++;
           if (format[i] == 's') {
-            for (; str[j] != ' '; j++) {
-                s[j] = str[j];
+            for (j--; str[j] != ' '; j++) {
+                 s[j] = str[j];
               }
-            res = s;
+            res = "s";
             continue;
-            } else if (format[i] == 'd') {
-              for (int k = 0, j = 0 ; str[j] != '\0'; j++) {
-                //*p = va_arg(arg, int);
-                if (str[j] > 47 && str[j] < 58) {
-                  p = (int)str[j];
-                  k++;
-                }
-              }
-              // res = (char*)p;
+            } else if (format[i] == 'd') {                  
+              p = s21_atoi(str, arr);
+              res = "p";
               continue;
             }
         } else { 
@@ -65,8 +81,9 @@ char *s21_sscanf(const char *str, const char *format, ...) {
 }
 
 int main() {
+
  res = s21_sscanf(tokenstring, "%s %d", s, &p);
- if (res != NULL) 
-  printf("%s", res);
+  if (res != NULL) 
+ printf("%s %d", s, p);
   return 0;
 }
